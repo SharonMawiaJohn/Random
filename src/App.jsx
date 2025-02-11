9f840902aba237946e51b75a45535fdc641c2a32
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-11 10:44:03 +0300

    adds all files created

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+function App(){
+  return <h1>My Name is Sharon</h1>
+
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import NavBar from './NavBar.jsx'
+createRoot(document.getElementById('root')).render(
+  <routes>
+    <NavBar />
+    <App />
+    
+  </routes>,
+)
