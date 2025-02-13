f97dcc68bf2545b7bc47210ef72acaa38c73544e
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-13 23:28:34 +0300

    Adds changes made

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,8 @@ const NavBar = () => {
                 <a href="/">Sharon</a>
             </div>
             <ul className="navbar-links">
-                <li><a href="/homelink">Home</a></li>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/other">Other</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/services">Services</a></li>
                 <li><a href="/contact">Contact</a></li>
